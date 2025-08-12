4ffa3686c6c51916010cd9d8bf04783c54ba2fca
llllIIIIllllIIII <g11016014@go.utaipei.edu.tw>
2025-08-12 20:28:09 +0800

    feat: add Vercel deployment configuration

--- next_config.js
+++ next_config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    esmExternals: false
+  }
 }
 
 module.exports = nextConfig
